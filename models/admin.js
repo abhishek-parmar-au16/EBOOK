dcc9f15fcb9113be7fcf870e68c96ec51f09a02e
abhishek-parmar-au16 <abhishekparmar220197@gmail.com>
2021-06-18 23:45:17 +0530

    done

---
+++ models/admin.js
@@ -0,0 +1,10 @@
+
+const mongoose = require("mongoose")
+const usersSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+
+
+});
+const Admindata = new mongoose.model("Admindata", usersSchema);
+module.exports = Admindata
